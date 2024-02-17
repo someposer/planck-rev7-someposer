faf7ea6bf2b6d3b027a7f97cbca1c894dc371d46
someposer <someposer@users.noreply.github.com>
2024-02-16 22:13:59 -0600

    Add tap dancing MEH/HYPE key.

--- planck/rev7/keymaps/someposer/keymap.c
+++ planck/rev7/keymaps/someposer/keymap.c
@@ -20,6 +20,30 @@ enum planck_layers { _QWERTY, _COLEMAK, _DVORAK, _LOWER, _RAISE, _PLOVER, _ADJUS
 
 enum planck_keycodes { QWERTY = SAFE_RANGE, COLEMAK, DVORAK, PLOVER, BACKLIT, EXT_PLV };
 
+// Tap Dance declarations
+enum {
+    TD_MEH_HYPE,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Mod, twice for Hype
+    [TD_MEH_HYPE] = ACTION_TAP_DANCE_DOUBLE(KC_MEH, KC_HYPE),
+};
+
+// Per key tapping term
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TD(TD_MEH_HYPE):
+            return 300;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
+#define MEHHYPE TD(TD_MEH_HYPE)
+
+// Define Layer keys
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
@@ -41,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_RSFT, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    MEHHYPE, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_RSFT, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak
