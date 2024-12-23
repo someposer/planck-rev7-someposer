f705bb00772fdcf685d3dd4893a28a64b9049b0e
someposer <someposer@users.noreply.github.com>
2024-12-23 15:03:06 -0600

    Fix complie issue with key_overrides

--- planck/rev7/keymaps/someposer/keymap.c
+++ planck/rev7/keymaps/someposer/keymap.c
@@ -66,9 +66,8 @@ tap_dance_action_t tap_dance_actions[] = {
 // const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
     // &delete_key_override,
-    NULL // Null terminate the array of overrides!
 };
 
 
